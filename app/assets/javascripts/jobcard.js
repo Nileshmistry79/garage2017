a338dc15f122d2e2f6a883e00b5fb09db2c9e2e2
Nilesh <nilesh.mistry79@gmail.com>
2017-01-19 19:54:09 +0530

    spinner

--- app/assets/javascripts/jobcard.js
+++ app/assets/javascripts/jobcard.js
@@ -15,6 +15,17 @@ var selected_resource_id = $(this).val();
           success: function(data){
           $('#jobcard_controller_RESULT').replaceWith(data);
            init_stock_lookup();
+          },
+          error: function(data){
+          $('#jobcard_controller_RESULT').replaceWith(data);
+          hide_spinner();
+           init_stock_lookup();
+          },
+          beforeSend: function(data){
+          	show_spinner();
+          },
+          complete: function(data){
+          	hide_spinner();
           }
   })
 });
