2b95abff3fb7ff3380d6f95ea70dd0515c3e6836
Nilesh <nilesh.mistry79@gmail.com>
2017-01-20 16:32:12 +0530

    Number of vehciles for each customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,5 @@ class Customer < ActiveRecord::Base
   validates :cust_mobile, presence: true, length: { :is => 10 }, uniqueness: true, numericality: true
   validates_format_of :cust_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   
+
 end  
